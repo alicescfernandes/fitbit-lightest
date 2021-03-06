cf5664d116382758b3b4dad431f6b05eec95f981
Alice Fernandes <alice.fernandes@sky.uk>
2021-03-06 18:46:08 +0000

    add travis; poke device

--- app/index.js
+++ app/index.js
@@ -1,31 +1,33 @@
-import document from "document";
-import { display } from "display";
+import document from 'document';
+import { display } from 'display';
 
-(function(){
-  let isOn = true;
-  let defaultDisplayValue = display.brightnessOverride
-  let defaultAutoOff = display.autoOff
-  let root = document.getElementById("background")
-  let background = document.getElementById("root")
-  
-  function changeDisplay(){
-    if(isOn){
-      background.class = "on"
-      background.animate("enable"); // Specify the name of the event to trigger
-      display.brightnessOverride = "max"
-    }else{
-      background.class = "off"
-      background.animate("disable"); // Specify the name of the event to trigger
-      display.brightnessOverride = defaultDisplayValue;      
-    }
-  }
-  
-  root.onmouseup =  function(){
-    isOn = !isOn;
-    changeDisplay();
-  }
-  
-  changeDisplay();
+(function () {
+	let isOn = true;
+	let defaultDisplayValue = display.brightnessOverride;
+	let defaultAutoOff = display.autoOff;
+	let root = document.getElementById('background');
+	let background = document.getElementById('root');
+	let interval = null;
+	function changeDisplay() {
+		if (isOn) {
+			background.class = 'on';
+			background.animate('enable'); // Specify the name of the event to trigger
+			display.brightnessOverride = 'max';
+			interval = setInterval(() => {
+				display.poke();
+			}, 50);
+		} else {
+			background.class = 'off';
+			background.animate('disable'); // Specify the name of the event to trigger
+			display.brightnessOverride = defaultDisplayValue;
+			clearInterval(interval);
+		}
+	}
 
-  
-})()
+	root.onmouseup = function () {
+		isOn = !isOn;
+		changeDisplay();
+	};
+
+	changeDisplay();
+})();
